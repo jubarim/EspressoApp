b3c15cef9d9c0485a3b01597ae7eafecaeefde62
Joao Assis <joao.assis@clara.team>
2026-04-25 09:25:04 -0300

    feat: update coffee bottom navigation icon.

--- app/src/main/java/org/juba/espressoapp/ui/main/AppDestinations.kt
+++ app/src/main/java/org/juba/espressoapp/ui/main/AppDestinations.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Coffee
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import org.juba.espressoapp.R
@@ -15,7 +15,7 @@ enum class AppDestinations(
     val startRoute: String,
 ) {
     SHOTS(R.string.tab_shots, Icons.AutoMirrored.Filled.List, "shots"),
-    COFFEE(R.string.tab_coffee, Icons.Default.Favorite, "coffee_home"),
+    COFFEE(R.string.tab_coffee, Icons.Default.Coffee, "coffee_home"),
     GEAR(R.string.tab_gear, Icons.Default.Build, "gear"),
     SETTINGS(R.string.tab_settings, Icons.Default.Settings, "settings"),
 }
