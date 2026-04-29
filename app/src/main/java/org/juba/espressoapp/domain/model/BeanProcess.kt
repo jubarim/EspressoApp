fb4e31353e0d1139f38397ddd79d823cb773db80
Joao Assis <joao.assis@clara.team>
2026-04-29 17:26:11 -0300

    feat: improve BeanProcess object.

--- app/src/main/java/org/juba/espressoapp/domain/model/BeanProcess.kt
+++ app/src/main/java/org/juba/espressoapp/domain/model/BeanProcess.kt
@@ -5,10 +5,12 @@ import org.juba.espressoapp.designsystem.SelectionOption
 /** Predefined espresso bean processing methods used in the coffee bean form. */
 object BeanProcess {
     val options: List<SelectionOption> = listOf(
-        SelectionOption("Washed", "Washed"),
-        SelectionOption("Natural", "Natural"),
-        SelectionOption("Honey", "Honey"),
-        SelectionOption("Anaerobic", "Anaerobic"),
-        SelectionOption("Wet-Hulled", "Wet-Hulled"),
+        SelectionOption("Washed", "Washed / Fully Washed"),
+        SelectionOption("Semi-Washed", "Semi-Washed (Despolpado)"),
+        SelectionOption("PulpedNatural", "Pulped Natural (CD)"),
+        SelectionOption("Honey", "Honey Process"),
+        SelectionOption("Natural", "Natural (Dry Process)"),
+        SelectionOption("Anaerobic", "Anaerobic Fermentation"),
+        SelectionOption("WetHulled", "Wet-Hulled (Giling Basah)"),
     )
 }
